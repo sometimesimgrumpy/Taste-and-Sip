da5f7bcae57758c32bbe8ff64372198d9024704a
Christina R <xsnachannell@gmail.com>
2022-04-04 23:12:30 -0700

    updates script to create elements and fill them with search results... not functional though

--- tastyScript.js
+++ tastyScript.js
@@ -5,14 +5,38 @@ const fetchOptions = {
         'X-RapidAPI-Key': 'fef51a61f7mshf38ff76f58269cdp15072cjsnea9cce855f99'
     }
 };
+
  var index = 0;
+ var userInput;
+
+
+function init() {
+    let submit = document.querySelector("#submitButton");
+    submit.addEventListener("click", function(event) {
+        event.preventDefault();
+        let userInputEl = document.querySelector("#ingInput");
+        var userInput = userInputEl.value;
+        console.log(userInput);
+        let data = await getRecipes(userInput);
+        console.log(data);
+        let organizedData = organizeData(data);
+        console.log(organizedData);
+        for (let i=0; i < organizedData.length; i++) {
+        createElements();
+        fillElements(data);
+        userInputEl.value="";
+        // window.location.href = "Url for search page";
+        }
+    });
+
+}
 
 async function getRecipes(searchInput) {
 
     const Url = 'https://tasty.p.rapidapi.com/recipes/list?from=' + index + "&size=5&q=" + searchInput;
     const response = await fetch(Url, fetchOptions);
     const rawData = await response.json();
-    console.log(rawData);
+    return rawData;
 }
 
 function organizeData (data) {
@@ -32,6 +56,24 @@ function organizeData (data) {
     return searchData;
 }
 
+function fillElements(data) {
+    // 9 total elements in search result div, so 0-8
+    for (let i = 0; i < data.length; i++) {
+        let searchResultsEl = document.querySelector("#dataDump").children[0].eq(i);
+        let dataArray = data[i];
+        for(let j = 0; j < dataArray.length; j++) {
+        searchResultsEl.children[4].textContent = dataArray[i];
+        searchResultsEl.children[5].textContent = dataArray[i];
+        let imgIcon = document.getElementById("");
+        imgIcon.setAttribute("href",dataArray[i]);
+        searchResultsEl.children[6].textContent = dataArray[i];
+        // searchResultsEl.children[].textContent = dataArray[i];
+        // searchResultsEl.children[i].textContent = dataArray[i];
+        // searchResultsEl.children[i].textContent = dataArray[i];
+        }
+    }
+}
+
 function createElements() {
     let mainDiv = document.createElement("div");
     let imgDiv = document.createElement("div");
@@ -43,16 +85,26 @@ function createElements() {
     let buttonDiv = document.createElement("div");
     let saveButton = document.createElement("div");
     let likeIcon = document.createElement("i");
+    let searchResultsEl = document.querySelector("#dataDump");
 
-    img.setAttribute("src", "")
+    img.setAttribute("class", "thumbImg");
+    likeIcon.setAttribute("class", "fa fa-heart");
+    likeIcon.setAttribute("aria-hidden", "true");
 
     imgDiv.appendChild(img);
+    textDiv.appendChild(recipeName);
+    textDiv.appendChild(descrip);
+    textDiv.appendChild(servings);
+    buttonDiv.appendChild(saveButton);
     mainDiv.appendChild(imgDiv);
     mainDiv.appendChild(textDiv);
     mainDiv.appendChild(buttonDiv);
+ 
 
+    searchResultsEl.appendChild(mainDiv);
 }
-// function ()
 
 
-getRecipes("Chicken, ham, sausage");
+init();
+
+
