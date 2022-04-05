a4302ffdb554a6bbcb0c3d7e249d7cda524e7de0
Christina R <xsnachannell@gmail.com>
2022-04-04 21:10:30 -0700

    updates script.js to assign data elements to variables and to create divs for search results

--- tastyScript.js
+++ tastyScript.js
@@ -16,13 +16,43 @@ async function getRecipes(searchInput) {
 }
 
 function organizeData (data) {
+    let searchData = [];
+    for (let i = 0; i < data.length; i++) {
+        
     const name = data[i].name;
     const descr = data[i].description;
     const img = data[i].thumbnail_url;
-    const yields = data[i].recipes
-    const ingredients = data[i].components;
+    const yields = data[i].yields;
+    const ingredients = data[i].sections[0].components;
+    const time = data[i].total_time_minutes;
+    const altTime = data[i].total_time_tier.tier;
+    const array = [name, descr, img, yields, ingredients, time, altTime];
+    searchData.push(array);
+    }
+    return searchData;
 }
-function ()
+
+function createElements() {
+    let mainDiv = document.createElement("div");
+    let imgDiv = document.createElement("div");
+    let img = document.createElement("img");
+    let textDiv = document.createElement("div");
+    let recipeName = document.createElement("h3");
+    let descrip = document.createElement("p");
+    let servings = document.createElement("p");
+    let buttonDiv = document.createElement("div");
+    let saveButton = document.createElement("div");
+    let likeIcon = document.createElement("i");
+
+    img.setAttribute("src", "")
+
+    imgDiv.appendChild(img);
+    mainDiv.appendChild(imgDiv);
+    mainDiv.appendChild(textDiv);
+    mainDiv.appendChild(buttonDiv);
+
+}
+// function ()
 
 
 getRecipes("Chicken, ham, sausage");
