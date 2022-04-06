dd7c7a7a57ecd4a4aecf1238d1db664966e431fc
Anita Chengalva <anita.chengalva@gmail.com>
2022-04-05 21:01:42 -0700

    getElementById into js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 var requestUrl = "https://thecocktaildb.com/api/json/v1/1/random.php";
 
-var responseText = document.getElementById("response-text");
+var responseText = document.getElementById("drinkName");
 
 console.log(drinkBtn(requestUrl))
+
+Object.onclick = function(){drinkBtn};
 
 function drinkBtn(requestUrl) {
     fetch(requestUrl)
@@ -18,7 +20,28 @@ function drinkBtn(requestUrl) {
     })
 }
 
+function displayCocktail(data) {
+    const drink = data.drinks[0];
+    const drink_div = document.getElementById("drinkName");
+
+    const drink_name = drink.strDrink;
+    const heading = document.createElement("h1");
+    heading.innerHTML = drink_name;
+    drink_div.appendChild(heading);
+
+    const drink_img = document.createElement("img");
+    drink_img.src = drink.strDrinkThumb;
+    drink_div.appendChild(drink_img);
+    document.body.style.backgroundImage = "url('" + drink.strDrinkThumb + "')";
+
+    // drink_info for cocktail ingredients
+
+}
+
 // drinkBtn(requestUrl);
 
 // buttonElement.addEventListener('click', function (event) {
 //    funcOutput.textContent = 'Element clicked through function!';
+
+// const drink_name = document.getElementById("drinkName");
+// const drink_info = document.getElementById("drinkInfo");
