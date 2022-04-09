4ac7d8edd43b5e111d633af921649378174fc92b
Anita Chengalva <anita.chengalva@gmail.com>
2022-04-09 12:22:00 -0700

    display ingredients with for loop

--- script.js
+++ script.js
@@ -3,65 +3,63 @@ var requestUrl = "https://thecocktaildb.com/api/json/v1/1/random.php";
 
 var responseText = document.getElementById("drinkName");
 
-// console.log(drinkBtn(requestUrl))
-
 function drinkBtn(requestUrl) {
     var data
     fetch(requestUrl)
     .then(function (response) {
         if (response.status === 200) {
             responseText.textContent;
-            console.log(response.status);
         }
         return response.json()
     })
     .then(function (response){
-        console.log(response);
         data = response;
         displayCocktail(data);
+        console.log(data)
     })
 }
 
 function displayCocktail(data) {
+    // pulls and displays drink name
     const drink = data;
     const drink_div = document.getElementById("drinkName");
-
-    const drink_name = drink.drinks[0].strDrink; 
-    console.log(drink_name);
+    const drink_name = drink.drinks[0].strDrink;
 
     drink_div.innerHTML = "";
     drink_div.append(drink_name);
 
-    // const heading = document.createElement("h1");
-    
-    // const drink_btn_result = document.getElementById("drinkBtnResult");
-    // heading.innerHTML = drink_btn_result;
-    // drink_btn_result.appendChild(heading);
-
+    // pulls and displays image of cocktail
     const drink_img = document.createElement("img");
     drink_img.src = drink.drinks[0].strDrinkThumb;
     drink_div.appendChild(drink_img);
 
+    // pulls and displays instructions to make cocktail
     const drink_instr = document.createElement("p");
     drink_instr.textContent = drink.drinks[0].strInstructions;
     drink_div.appendChild(drink_instr);
 
-    const drink_ingr_list = document.createElement("ol");
-    let drink_ingr = "empty"
-    while (drink_ingr !== null ) {
-        const list_item
+
+    // pulls and displays all cocktail ingredients
+    const drink_ingr_list = document.createElement("ul");
+    drink_div.appendChild(drink_ingr_list);
+
+    const list_item = Object.keys(drink)
+    .filter(function (ingredients) {
+        return ingredients.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+        if (drink[ingredient] != null) {
+            ingredients[ingredient] = drink[ingredient];
+        }
+        return ingredients;
+    }, {});
+
+
+    for (let key in list_item) {
+        let value = list_item[key];
+        listItem = document.createElement("li");
+        listItem.innerHTML = value;
+        drink_ingr_list.appendChild(listItem); 
     }
 
-   // document.body.style.backgroundImage = "url('" + drink.drinks[0].strDrinkThumb + "')";
-
-    // drink_info for cocktail ingredients into <h2>
-
 }
-
-// drinkBtn(requestUrl);
-
-// buttonElement.addEventListener('click', function (event) {
-//    funcOutput.textContent = 'Element clicked through function!';
-
-// const drink_name = document.getElementById("drinkName");
-// const drink_info = document.getElementById("drinkInfo");
